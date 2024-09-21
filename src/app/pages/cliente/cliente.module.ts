2ec843a4fe7f390dfb7e411df2830c7d72335c6f
Nicoly-Almeida <nicolyejady@gmail.com>
2024-09-21 13:32:50 -0300

    listagem clientes

--- src/app/pages/cliente/cliente.module.ts
+++ src/app/pages/cliente/cliente.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { ListagemClienteComponent } from './listagem-cliente/listagem-cliente.component';
 
 
 
 @NgModule({
   declarations: [
-    CadastroClienteComponent
+    CadastroClienteComponent,
+    ListagemClienteComponent
   ],
   imports: [
     CommonModule,
@@ -28,6 +30,9 @@ import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.co
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [provideNgxMask()],
-  exports: [ CadastroClienteComponent ]
+  exports: [ 
+    CadastroClienteComponent,
+    ListagemClienteComponent
+  ]
 })
 export class ClienteModule { }
---
+++ src/app/pages/cliente/listagem-cliente/listagem-cliente.component.ts
@@ -0,0 +1,73 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Cliente } from 'src/app/shared/models/cliente';
+import { ClienteService } from 'src/app/shared/services/cliente/cliente.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-listagem-cliente',
+  templateUrl: './listagem-cliente.component.html',
+  styleUrls: ['./listagem-cliente.component.scss']
+})
+export class ListagemClienteComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'status', 'funcoes'];
+  dataSource = new MatTableDataSource<Cliente>;
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private clienteService: ClienteService){
+  }
+
+
+  ngAfterViewInit() {
+    this.listarClientes(1, 5)
+  }
+
+
+  listarClientes(page: number, pageSize: number) {
+    this.clienteService.listar_paginado(page, pageSize).subscribe(clientes => {
+      this.dataSource.data = clientes;
+    });
+  }
+
+
+  onPageChange(event: PageEvent) {
+    const pageIndex = event.pageIndex + 1;
+    const pageSize = event.pageSize;
+    this.listarClientes(pageIndex, pageSize);
+  }
+
+
+  deletarCLiente(id: number){
+    Swal.fire({
+      title: 'Você tem certeza que deseja deletar?',
+      text: "Não tem como reverter essa ação",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'red',
+      cancelButtonColor: 'grey',
+      confirmButtonText: 'Deletar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.deletar(id).subscribe({
+          next: () => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Sucesso',
+              text: 'Cliente deletado com sucesso!',
+              showConfirmButton: false,
+              timer: 1500
+            })
+            this.listarClientes(1,5)
+          },
+          error: (error) => {
+            console.error(error)
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Erro ao deletar cliente!',
+            })
+          }})
+      }})}
+}
